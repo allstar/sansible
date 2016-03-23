18ea69fd696380e37b62f7604c52dd93536dd9e7
Andrea Fiore <andrea.fiore@composable.io>
2016-03-23 13:53:34 +0000

    Add handlers

--- ansible/src/test/scala/ansible/JsonEncodersSpec.scala
+++ ansible/src/test/scala/ansible/JsonEncodersSpec.scala
@@ -13,15 +13,23 @@ class JsonEncodersSpec extends FreeSpec {
     "can serialise a playbook" in {
       val f = File(path="/foo/bar", state=Some(File.State.file))
       val t1 = Task("create foo/bar", f, Task.Options(tags = Some(List("tag1", "tag2"))))
-      val t2 = Task("ping host", Ping())
-      val pb = Playbook(List(HostPattern("example-host")), t1 :: t2 :: Nil, Playbook.Options(connection = Some("local")))
+      val t3 = Task("reboot system", Command("reboot"))
+      val t2 = Task("ping host", Ping(), Task.Options(notifyTask = Some(t3)))
+      val pb = Playbook(
+        hosts = List(HostPattern("example-host")),
+        tasks = t1 :: t2 :: Nil,
+        handlers = t3 :: Nil,
+        options = Playbook.Options(connection = Some("local")))
 
       val json = Parse.parse("""{
           |  "hosts": ["example-host"],
           |  "connection": "local",
           |  "tasks": [
           |    {"name": "create foo/bar", "file": {"args": {"state": "file", "path": "/foo/bar"}}, "tags": ["tag1", "tag2"]},
-          |    {"name": "ping host", "ping": {"args": {}}}
+          |    {"name": "ping host", "ping": {"args": {}}, "notify": "reboot system"}
+          |  ],
+          |  "handlers": [
+          |    {"name": "reboot system", "command": {"args": {"free_form": "reboot"}}}
           |  ]
           |}""".stripMargin)
 
