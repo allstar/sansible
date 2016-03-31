140bce595d0045f238dafddd7f6af4a5e6f38e3a
Andrea Fiore <andrea.fiore@composable.io>
2016-03-31 17:29:31 +0100

    allow to supply cli options to the ansible runner

--- ansible/src/main/scala/ansible/Runner.scala
+++ ansible/src/main/scala/ansible/Runner.scala
@@ -7,25 +7,18 @@ import scala.sys.process.{Process, ProcessIO}
 import scala.io.Source
 
 object Runner {
-  def runPlaybook(inv: Inventory)(pb: Playbook): Unit = {
+  def runPlaybook(inv: Inventory)(pb: Playbook, opts: Option[String] = None): Unit = {
     val invFile = File.newTemporaryFile("ansible-inventory")
-    val pbFile  = File.newTemporaryFile("ansible-playbook", "yml")
-    val pio = new ProcessIO(
-      _ => (),
+    val pbFile  = File.newTemporaryFile("ansible-playbook", ".yml")
+    val pio = new ProcessIO(_ => (),
       out  => Source.fromInputStream(out).getLines.foreach(println),
       err => Source.fromInputStream(err).getLines.foreach(System.err.println)
     )
-
+    val cmd = s"ansible-playbook ${opts.getOrElse("")} -i ${invFile.path} ${pbFile.path}"
+    val env = Seq("ANSIBLE_FORCE_COLOR" -> "true")
 
     invFile.write(inv.iniEncode)
     pbFile.write(YAML.fromPlaybook(pb))
-
-    println(s"ansible-playbook -i ${invFile.path} ${pbFile.path}")
-
-    Process(
-      s"ansible-playbook -vvv -i ${invFile.path} ${pbFile.path}",
-      cwd = None,
-      Seq("ANSIBLE_FORCE_COLOR" -> "true"): _*
-    ).run(pio)
+    Process(cmd, cwd = None, env: _*).run(pio)
   }
 }
