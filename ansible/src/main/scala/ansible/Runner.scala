ac42f1931b6be6896ea44a7606894732d55fe440
Andrea Fiore <andrea.fiore@composable.io>
2016-04-08 16:24:06 +0100

    use a standard logger in ansuble runner

--- ansible/src/main/scala/ansible/Runner.scala
+++ ansible/src/main/scala/ansible/Runner.scala
@@ -1,25 +1,34 @@
 package ansible
 
-import better.files.File
-import IniEncode._
-import IniEncoders._
-import scala.sys.process.{Process, ProcessIO}
 import scala.io.Source
+import scala.sys.process.{Process, ProcessIO}
 
-object Runner {
+import ansible.IniEncode._
+import ansible.IniEncoders._
+import better.files.File
+import com.typesafe.scalalogging.LazyLogging
+
+object Runner extends LazyLogging {
   def runPlaybook(inv: Inventory)(pb: Playbook, opts: Option[String] = None): Unit = {
     val invFile = File.newTemporaryFile("ansible-inventory")
     val pbFile  = File.newTemporaryFile("ansible-playbook", ".yml")
-    val pio = new ProcessIO(_ => (),
+
+    val pio = new ProcessIO(
+      _ => (),
       out  => Source.fromInputStream(out).getLines.foreach(println),
       err => Source.fromInputStream(err).getLines.foreach(System.err.println)
     )
+
     val cmd = s"ansible-playbook ${opts.getOrElse("")} -i ${invFile.path} ${pbFile.path}"
     val env = Seq("ANSIBLE_FORCE_COLOR" -> "true")
+    val process = Process(cmd, cwd = None, env: _*).run(pio)
 
     invFile.write(inv.iniEncode)
     pbFile.write(YAML.fromPlaybook(pb))
-    println(cmd)
-    Process(cmd, cwd = None, env: _*).run(pio)
+    logger.info(cmd)
+
+    val exitCode = process.exitValue()
+    logger.info(s"run completed with exit code: $exitCode")
+    process.destroy()
   }
 }
